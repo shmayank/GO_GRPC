8c50b81e07ad29168b2a8704a4ab43c27f9155aa
Monu Sharma <monu6597@gmail.com>
2021-01-10 17:30:12 +0530

    Add files via upload

---
+++ grpc_blockchain_eg/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/shmayank/GO_GRPC/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello there!............")
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalf("unable to listen on 8080 port: %v", err)
+	}
+
+	srv := grpc.NewServer()
+
+	proto.RegisterBlockchainServer(srv, &Server{})
+	srv.Serve(listener)
+
+}
+
+type Server struct{}
+
+func (s *Server) AddBlock(context.Context, *proto.AddBlockRequest) (*proto.AddBlockResponse, error) {
+	return new(proto.AddBlockResponse), nil
+}
+
+func (s *Server) GetBlockChain(context.Context, *proto.GetBlockRequest) (*proto.GetBlockResponse, error) {
+	return new(proto.GetBlockResponse), nil
+}
